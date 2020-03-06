b9043f88dde2f484973e5f317c8732f696618869
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-06 23:21:38 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,7 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[4.2]
+  def change
+		create_table :students do |x|
+			x.string :name
+		end
+	end
 end
