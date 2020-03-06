1cf5366f6cebc31fe7c1e26c545bbad53a660221
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-03-06 23:21:27 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,4 +1,4 @@
-class ChangeDatatypeForBirthdate < ActiveRecord::Migration
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[4.2]
 	def change
 		change_column :students, :birthdate, :datetime
 	end
